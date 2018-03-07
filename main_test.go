596d7bcfbab6675ea5401e12592200dc6e78124b
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2018-03-07 23:03:56 +0900

    update

--- main_test.go
+++ main_test.go
@@ -8,3 +8,10 @@ func TestHello2(t *testing.T) {
 		t.Fatal("Error: Hello func test code. return miss")
 	}
 }
+
+func TestHello3(t *testing.T) {
+	s := Hello()
+	if s != "Hello World2"{
+		t.Fatal("Error: Hello func test code. return miss")
+	}
+}
